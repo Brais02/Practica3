36478f9d27c4967b402cc77ca62207ff58f268ca
Brais02 <74008056+Brais02@users.noreply.github.com>
2021-05-24 20:20:26 +0200

    Update CajeroAutomatico.java

--- CajeroAutomatico.java
+++ CajeroAutomatico.java
@@ -1,15 +1,55 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CajeroAutomatico {
+    static Scanner src = new Scanner(System.in);
     int idUltCaj = 0;
     int idCaj = 0;
-    int Billetes[];
-    ArrayList<String> listaTarjeta = new ArrayList<String>();
+    int Billetes[][];
+    ArrayList<Tarjeta> listaTarjeta = new ArrayList<Tarjeta>();
 
     void mostrarCajero(){
-        for (int i = 0; this.Billetes.length > i; i++) {
-        System.out.println(this.Billetes[i]);
+        for (int i = 0; 7 > i; i++) {
+        System.out.println(this.Billetes[i][1]+ " billetes de " + this.Billetes[i][0]);
         }
+    }
+    void sacarDinero(){
+        System.out.println("Introduce tu NIF:");
+        String nif = src.nextLine();
+        System.out.println("Introduce tu PIN:");
+        int pin = src.nextInt();
+        boolean usuarioigual = comprobarUsuario(this.getListaTarjeta(), nif, pin);
+        if (usuarioigual == true) {
+            /*Ahora si que deberia de empezar a sacar cuartos en orden de mas a menos*/
+            System.out.println("Cuanto dinero quieres sacar?");
+        } else {
+            System.out.println("No has introducido un NIF o PIN correcto");
+        }
+    }
+
+    public boolean comprobarUsuario(ArrayList<Tarjeta> tarjeta, String id, int con){
+    /*Aqui lo que hace falta es comprobar que el valor de la tarjeta dentro del array de tarjetas es igual al parametro
+    que sera el nif y el pin en la tarjeta.*/
+        if (id != tarjeta.get(int)){
+            System.out.println("No has introducido el NIF correcto");
+            return false;
+
+        } else {
+
+            if (con == tarjeta.indexOf()) {
+                return true;
+
+            } else {
+                System.out.println("No has introducido el PIN correcto");
+                return false;
+            }
+            }
+
+    }
+
+
+    public CajeroAutomatico() {
+        this.setIdCaj(idUltCaj++);
     }
 
     public int getIdUltCaj() {
@@ -28,19 +68,19 @@ public class CajeroAutomatico {
         this.idCaj = idCaj;
     }
 
-    public int[] getBilletes() {
+    public int[][] getBilletes() {
         return Billetes;
     }
 
-    public void setBilletes(int[] Billetes) {
+    public void setBilletes(int[][] Billetes) {
         this.Billetes = Billetes;
     }
 
-    public ArrayList<String> getListaTarjeta() {
+    public ArrayList<Tarjeta> getListaTarjeta() {
         return listaTarjeta;
     }
 
-    public void setListaTarjeta(ArrayList<String> listaTarjeta) {
+    public void setListaTarjeta(ArrayList<Tarjeta> listaTarjeta) {
         this.listaTarjeta = listaTarjeta;
     }
 }
