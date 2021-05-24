9dc38a34df3de55067e48f1a9fb64b4e28630010
Brais02 <74008056+Brais02@users.noreply.github.com>
2021-05-24 20:18:05 +0200

    Update TarjetaDebito.java

--- TarjetaDebito.java
+++ TarjetaDebito.java
@@ -1,6 +1,14 @@
 public class TarjetaDebito extends Tarjeta {
     int saldoDisponible;
 
+    public TarjetaDebito() {
+    }
+
+    public TarjetaDebito(String nif, int pin, String nombre, String apellido, int saldoDisponible) {
+        super(nif, pin, nombre, apellido);
+        this.saldoDisponible = saldoDisponible;
+    }
+
     @Override
     void mostrarTarjeta() {
         super.mostrarTarjeta();
