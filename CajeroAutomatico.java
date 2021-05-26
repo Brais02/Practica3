7a44e76abbc941d7048f1d84dc6d598c91e926f0
Brais02 <74008056+Brais02@users.noreply.github.com>
2021-05-26 20:52:32 +0200

    Update CajeroAutomatico.java

--- CajeroAutomatico.java
+++ CajeroAutomatico.java
@@ -22,29 +22,42 @@ public class CajeroAutomatico {
         if (usuarioigual == true) {
             /*Ahora si que deberia de empezar a sacar cuartos en orden de mas a menos*/
             System.out.println("Cuanto dinero quieres sacar?");
+            int dinerosacar = src.nextInt();
+            
+
+
         } else {
-            System.out.println("No has introducido un NIF o PIN correcto");
+            System.out.println("No has introducido los datos de NIF o PIN correcto");
         }
     }
 
-    public boolean comprobarUsuario(ArrayList<Tarjeta> tarjeta, String id, int con){
-    /*Aqui lo que hace falta es comprobar que el valor de la tarjeta dentro del array de tarjetas es igual al parametro
-    que sera el nif y el pin en la tarjeta.*/
-        if (id != tarjeta.get(int)){
-            System.out.println("No has introducido el NIF correcto");
-            return false;
-
-        } else {
-
-            if (con == tarjeta.indexOf()) {
-                return true;
-
+    /*El metodo comprobarUsuario, devuelve un booleano y se introducen los parametros de la ArrayList de Tarjeta, un
+    * String que se llamara id y un integer que se llamara con, estos últimos definidos por el usuario al llamar al
+    * metodo, para ello mientras que la variable integer i sea menor que el total de Tarjetas que tiene el ArrayList
+    * y al mismo tiempo, mientras que la variable boolean coincide sea false, se va a mantener realizando una comprobación
+    * para asegurarse de que los datos del NIF de la tarjeta sean iguales a id (para ello se usa equals por ser String) y
+    * si los datos de PIN de la tarjeta son iguales a con (para ello se usan los iguales "==" al ser numerico), y en caso
+    * de que este correcto te mostrara un mensaje y de cambiar coincide a true, en caso de ser negativo te devolvera
+    * false y siempre sumara +1 a la variable i, además que al final devolvera el valor que tenga coincide.*/
+    public boolean comprobarUsuario(ArrayList<Tarjeta> tarjeta, String id, int con) {
+        boolean coincide = false;
+        int i = 0;
+        while (i < tarjeta.size() && coincide == false) {
+            if (tarjeta.get(i).getNif().equals(id)) {
+                if (tarjeta.get(i).getPin() == con) {
+                    System.out.println("Has introducido los datos de manera correcta");
+                    System.out.println("Hola "+ tarjeta.get(i).getNombre());
+                    coincide = true;
+                } else {
+                    System.out.println("El Pin introducido no es correcto!");
+                    return false;
+                }
             } else {
-                System.out.println("No has introducido el PIN correcto");
-                return false;
+                System.out.println("El NIF introducido es incorrecto!");
             }
-            }
-
+            i++;
+        }
+        return coincide;
     }
 
 
@@ -84,3 +97,4 @@ public class CajeroAutomatico {
         this.listaTarjeta = listaTarjeta;
     }
 }
+
