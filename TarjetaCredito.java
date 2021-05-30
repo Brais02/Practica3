f571c11cd45cb8f23463618b59f21e595eba75cc
Brais02 <74008056+Brais02@users.noreply.github.com>
2021-05-30 23:53:33 +0200

    Update TarjetaCredito.java

--- TarjetaCredito.java
+++ TarjetaCredito.java
@@ -2,20 +2,27 @@ public class TarjetaCredito extends Tarjeta {
     int saldoDisponible;
     int creditoDisponible;
 
+    /*Constructor de la clase sin parametros por si se instancia vacia, que no de
+     * error y poder llamar a algun constructor*/
     public TarjetaCredito() {
     }
 
+    /*Constructor de la clase con los parametros heredados de Tarjeta y los propios*/
     public TarjetaCredito(String nif, int pin, String nombre, String apellido, int saldoDisponible, int creditoDisponible) {
         super(nif, pin, nombre, apellido);
         this.saldoDisponible = saldoDisponible;
         this.creditoDisponible = creditoDisponible;
     }
 
+    /*Muestra por pantalla el valor de los atributos del constructor además
+     * de los heredados de Tarjeta*/
     @Override
     void mostrarTarjeta() {
         super.mostrarTarjeta();
-        System.out.println(this.saldoDisponible);
-        System.out.println(this.creditoDisponible);
+        System.out.println("|                                 |");
+        System.out.println("|        TARJETA DE CREDITO:      |");
+        System.out.println("| El saldo es: " + this.saldoDisponible + "               |");
+        System.out.println("| El credito es: " + this.creditoDisponible + "             |");
     }
 
     public int getSaldoDisponible() {
